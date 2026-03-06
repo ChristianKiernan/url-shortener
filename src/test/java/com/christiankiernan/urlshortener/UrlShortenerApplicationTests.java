4328c8cb3cfb2936f044b192ee7faba8b7e91267
christiankiernan <clkiernan1@gmail.com>
2026-03-06 13:01:09 -0500

    test: wire Testcontainers PostgreSQL via Spring Boot @ServiceConnection

--- src/test/java/com/christiankiernan/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/christiankiernan/urlshortener/UrlShortenerApplicationTests.java
@@ -2,12 +2,25 @@ package com.christiankiernan.urlshortener;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @TestConfiguration(proxyBeanMethods = false)
+    static class ContainerConfig {
+        @Bean
+        @ServiceConnection
+        PostgreSQLContainer<?> postgres() {
+            return new PostgreSQLContainer<>("postgres:16");
+        }
     }
 
 }
