5468d60b4d31bf517637eb99c646e69498bb8c8c
christiankiernan <clkiernan1@gmail.com>
2026-03-10 16:15:17 -0400

    test: add unit tests for RedirectController

---
+++ src/test/java/com/christiankiernan/urlshortener/controllers/RedirectControllerTest.java
@@ -0,0 +1,49 @@
+package com.christiankiernan.urlshortener.controllers;
+
+import com.christiankiernan.urlshortener.exceptions.GlobalExceptionHandler;
+import com.christiankiernan.urlshortener.exceptions.NotFoundException;
+import com.christiankiernan.urlshortener.models.ShortenedUrl;
+import com.christiankiernan.urlshortener.services.UrlShortenerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({RedirectController.class, GlobalExceptionHandler.class})
+class RedirectControllerTest {
+
+    private static final String TEST_URL = "https://example.com";
+    private static final String TEST_CODE = "abc123";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UrlShortenerService service;
+
+    @Test
+    void redirect_returns302WithLocationHeader() throws Exception {
+        ShortenedUrl entity = new ShortenedUrl();
+        entity.setUrl(TEST_URL);
+        entity.setShortCode(TEST_CODE);
+        when(service.getByShortCode(TEST_CODE)).thenReturn(entity);
+
+        mockMvc.perform(get("/{code}", TEST_CODE))
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location", TEST_URL));
+    }
+
+    @Test
+    void redirect_returns404WhenCodeNotFound() throws Exception {
+        when(service.getByShortCode(TEST_CODE)).thenThrow(new NotFoundException(TEST_CODE));
+
+        mockMvc.perform(get("/{code}", TEST_CODE))
+                .andExpect(status().isNotFound());
+    }
+}
