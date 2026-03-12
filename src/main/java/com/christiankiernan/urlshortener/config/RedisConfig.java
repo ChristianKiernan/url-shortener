b355b4fdf999c6b2405642081c398c7e0a3be5b7
christiankiernan <clkiernan1@gmail.com>
2026-03-12 14:32:54 -0400

    feat: integrate Redis for caching with configuration and dependencies

---
+++ src/main/java/com/christiankiernan/urlshortener/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.christiankiernan.urlshortener.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.christiankiernan.urlshortener.dto.ShortenedUrlResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        JsonRedisSerializer<ShortenedUrlResponse> serializer = buildSerializer();
+
+        RedisCacheConfiguration urlsConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+
+        RedisCacheConfiguration statsConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .withCacheConfiguration("urls", urlsConfig)
+                .withCacheConfiguration("stats", statsConfig)
+                .build();
+    }
+
+    private JsonRedisSerializer<ShortenedUrlResponse> buildSerializer() {
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new JsonRedisSerializer<>(mapper, ShortenedUrlResponse.class);
+    }
+}
